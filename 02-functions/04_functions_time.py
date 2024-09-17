0a8fca7a567adbd1c798a550ce33104aca7bb9fc
Marc <48025136+Craithez@users.noreply.github.com>
2024-09-17 19:20:43 +0200

    Update 04_functions_time.py

--- 02-functions/04_functions_time.py
+++ 02-functions/04_functions_time.py
@@ -1,16 +1,14 @@
 from datetime import datetime
 
 def aktuelles_datum_und_zeit():
-    # Aktuelles Datum und Zeit abrufen
     jetzt = datetime.now()
     
-    # Formatieren des Datums und der Zeit
     formatiert = jetzt.strftime("%A, %d.%m.%Y – %H:%M:%S")
     
-    # Ausgabe des formatierten Datums und Zeitstempels
+    # Ausgabe 
     print(formatiert)
 
-# Funktion aufrufen
+# Aufrufen
 aktuelles_datum_und_zeit()
 
 
