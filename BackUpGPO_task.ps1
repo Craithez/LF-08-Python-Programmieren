eec430fc5efbbc32a657c54048c0edc44aac3ecd
Marc <48025136+Craithez@users.noreply.github.com>
2024-10-09 14:47:21 +0200

    Update BackUpGPO_task.ps1

--- BackUpGPO_task.ps1
+++ BackUpGPO_task.ps1
@@ -1,3 +1,14 @@
-# Sammelt alle GPo's.
-Get-GPO -All | Backup-GPO -Path C:\Daten\GPOBackups\$(get-date -format 'HH:mm:ss dd-MM-yyyy')
+# Marc Lo Verde, LAV
 
+$Datum = (Get-Date -Format d.M.yyyy__H_mm_s) 
+# Bodenlos das ich das Datum nicht mit Leeschritt angeben kann aber egal.
+If ((Test-Path -Path C:\Backups\GPO-Backup)) {
+    If (Get-Module -ListAvailable -Name GroupPolicy) {
+        New-Item -Path "C:\Backups\GPO-Backup\$Datum" -ItemType directory
+        Backup-GPO -All -Path "C:\Backups\GPO-Backup\$Datum"
+    }
+    
+    Else {
+        Write-Warning "Fehler!"
+    }
+}
