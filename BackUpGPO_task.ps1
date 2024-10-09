96e1a190b5bb37ce7262ba22c3f9abebb1fe0f48
Marc <48025136+Craithez@users.noreply.github.com>
2024-10-09 13:03:55 +0200

    Update BackUpGPO_task.ps1

--- BackUpGPO_task.ps1
+++ BackUpGPO_task.ps1
@@ -1,21 +1,4 @@
 # Sammelt alle GPo's.
-# Get-GPO -All | Backup-GPO -Path C:\Daten\GPOBackup
 
-$GPOInfo = Get-GPO -All -Domain $domain -Server $Server
-$Date = Get-Date -UFormat "%d-%m-%Y"
-$UpdatedPath = "$path\$date"
-New-item $UpdatedPath -ItemType directory | Out-Null
+Get-GPO -All | Backup-GPO -Path C:\Daten\GPOBackups\$(get-date -format 'HH:mm:ss dd-MM-yyyy')
 
-ForEach ($GPO in $GPOInfo) {
-
-    #Assign temp variables for various parts of GPO data
-    Write-Host "Backing up GPO named: " $GPO.Displayname
-    $BackupInfo = Backup-GPO -Name $GPO.DisplayName -Domain $Domain  path $UpdatedPath -Server $Server
-    $GpoBackupID = $BackupInfo.ID.Guid
-    $GpoName = $BackupInfo.DisplayName
-    $CurrentFolderName = $UpdatedPath + "\" + "{"+ $GpoBackupID + "}"
-    $NewFolderName = $UpdatedPath + "\" + $GPOName
-
-    #Rename the newly created GPO backup subfolder from its GPO ID to the GPO Displayname + GUID
-    rename-item $CurrentFolderName -newname $NewFolderName
-}
