20906d8ae45f589bfd5e3b7acfbd12ff4aaa2a80
Marc <48025136+Craithez@users.noreply.github.com>
2024-10-28 12:10:18 +0100

    Update 05_functions_liste.py

--- 02-functions/05_functions_liste.py
+++ 02-functions/05_functions_liste.py
@@ -1,7 +1,4 @@
 def addiere_listen(liste1, liste2):
-    if len(liste1) != 3 or len(liste2) != 3:
-        raise ValueError("Beide Listen müssen genau drei Elemente enthalten.")
-    
     # Liste + Liste
     ergebnis = [a + b for a, b in zip(liste1, liste2)]
     
