2a8dd2418415eb014d9163c9160daff1830db359
Marc <48025136+Craithez@users.noreply.github.com>
2024-11-18 12:16:25 +0100

    Update 07_functions_1cidr.py

--- 02-functions/07_functions_1cidr.py
+++ 02-functions/07_functions_1cidr.py
@@ -6,8 +6,8 @@ def anzahl_hosts(cidr_suffix):
     host_bits = 32 - cidr_suffix
     # Anzahl der möglichen Hosts
     anzahl_hosts = (2 ** host_bits) - 2  # Abzug der Netzwerk- und Broadcast-Adresse
+    #oder (host_bits ** 2) - 2
     
-    return anzahl_hosts
-
+    return anzahl_hosts    # Oder mit print und dann unten aufrufen mit anzahl_hosts(6)
 # Einfach Zahl ändern. Max sollte bekannt sein, alles über 31 produziert "falsche" Ergebnisse.
-print(anzahl_hosts(29))  
+print(anzahl_hosts(6))
