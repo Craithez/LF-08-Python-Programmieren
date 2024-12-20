36f78ecd2bc948a2dcf944caac8aac9d16bcae40
Marc <48025136+Craithez@users.noreply.github.com>
2024-12-20 08:03:51 +0100

    Update inventarisierungAD.ps1

--- inventarisierungAD.ps1
+++ inventarisierungAD.ps1
@@ -4,3 +4,9 @@ Select-Object @{Name="ComputerName";Expression={$_.Name}},
               @{Name="OSVersion";Expression={$_.OperatingSystemVersion}},
               @{Name="IP";Expression={$_.IPv4Address}} |
 Export-CSV -Path "C:\Test\ADcomputerslist.csv" -NoTypeInformation -Encoding UTF8
+
+
+#Notiz 
+#Get-Module -ListAvailable ActiveDirectory
+#Import-Module ActiveDirectory
+#Und es funktioniert trotzdem nur im ISE.  ¯\_(ツ)_/¯ 
