6264166ded4bb790a904228832b0798f0efb29b5
Marc <48025136+Craithez@users.noreply.github.com>
2024-12-20 09:47:48 +0100

    Update inventarisierungAD.ps1

--- inventarisierungAD.ps1
+++ inventarisierungAD.ps1
@@ -2,8 +2,9 @@ Get-ADComputer -Filter * -Property * |
 Select-Object @{Name="ComputerName";Expression={$_.Name}},
               @{Name="OS";Expression={$_.OperatingSystem}},
               @{Name="OSVersion";Expression={$_.OperatingSystemVersion}},
+              @{Name="IP";Expression={$_.IPv4Address}},
               @{Name="IP";Expression={$_.IPv4Address}} |
-Export-CSV -Path "C:\Test\ADcomputerslist.csv" -NoTypeInformation -Encoding UTF8
+Export-CSV -Path "C:\Temp\ADcomputerslist.csv" -NoTypeInformation -Encoding UTF8
 
 
 #Notiz 
